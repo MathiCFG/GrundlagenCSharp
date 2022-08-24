1a5856184657d6d63b6f4ab6adbe69688fdf5ce0
MathiCFG <105783762+MathiCFG@users.noreply.github.com>
2022-08-24 14:46:43 +0200

    Update Program.cs

--- 1_UebungenVerzweigungen/Program.cs
+++ 1_UebungenVerzweigungen/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+int x = 9, z = 6; double y = 8;
+Console.WriteLine(x-- == y);                    //False
+Console.WriteLine(x==(int)y);                   //True
+Console.WriteLine("{0:F1}",0.123);              //0,1
+Console.WriteLine(z/y);                         //0,75
+Console.WriteLine(x-=10);                       //-2
+Console.WriteLine(x-- == y & true);             //False
+Console.WriteLine(!(z == 6) | false);           //False
+Console.WriteLine((x*=-2) == z);                //True
+*/
+
+Console.WriteLine("(1) Umrechnung von Celsius nach Kelvin"+
+    "\n(2) Umrechnung von Celsius nach Fahrenheit" +
+    "\n(3) Umrechnung von Kelvin nach Celsius" +
+    "\n(4) Umrechnung von Kelvin nach Fahrenheit");
+
+Console.WriteLine("Treffe eine Auswahl der Umrechnung:");
+int auswahl = Convert.ToInt16(Console.ReadLine());  //Auswahl variable festlegen
+
+switch (auswahl)
+{
+    case 1:
+        Console.WriteLine("Gebe deine Celsius Zahl an: ");
+        double Celsius = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(Celsius+" C° in Kelvin ergibt: "+(Celsius+273.15)+" Kelvin.");
+        break;
+    
+    case 2:
+        Console.WriteLine("Gebe deine Celsius Zahl an: ");
+        double Celsius1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(Celsius1+" C° in Fahrenheit ergibt: "+(Celsius1*1,8+32)+" Kelvin.");
+        break;
+
+}
