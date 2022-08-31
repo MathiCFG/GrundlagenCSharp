3a83fffebc397f39b10a1921e94d9b0609fbe6ff
MathiCFG <105783762+MathiCFG@users.noreply.github.com>
2022-08-31 14:12:35 +0200

    Update Program.cs

--- 1_UebungenVerzweigungen/Program.cs
+++ 1_UebungenVerzweigungen/Program.cs
@@ -17,19 +17,33 @@ Console.WriteLine("(1) Umrechnung von Celsius nach Kelvin"+
 
 Console.WriteLine("Treffe eine Auswahl der Umrechnung:");
 int auswahl = Convert.ToInt16(Console.ReadLine());  //Auswahl variable festlegen
+string er = "ERROR: ";
 
 switch (auswahl)
 {
     case 1:
-        Console.WriteLine("Gebe deine Celsius Zahl an: ");
+        Console.WriteLine("Gebe deine Celsius Zahl ein: ");
         double Celsius = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine(Celsius+" C° in Kelvin ergibt: "+(Celsius+273.15)+" Kelvin.");
+        Console.WriteLine(Celsius+" C° nach Kelvin ergibt: "+(Celsius + 273.15)+" Kelvin.");
         break;
     
     case 2:
-        Console.WriteLine("Gebe deine Celsius Zahl an: ");
+        Console.WriteLine("Gebe deine Celsius Zahl ein: ");
         double Celsius1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine(Celsius1+" C° in Fahrenheit ergibt: "+(Celsius1*1,8+32)+" Kelvin.");
+        Console.WriteLine(Celsius1+" C° nach Fahrenheit ergibt: "+(Celsius1 * 1,8 + 32)+" Fahrenheit.");
+        break;    
+    
+    case 3:
+        Console.WriteLine("Gebe deine Kelvin Zahl ein: ");
+        double Kelvin = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(Kelvin+" Kelvin nach Celsius ergibt: "+(Kelvin - 273.15)+" Celsius.");
+        break;
+
+    case 4:
+        Console.WriteLine("Gebe deine Kelvin Zahl ein: ");
+        double Kelvin1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(Kelvin1 + " Kelvin nach Fahrenheit ergibt: " + ((Kelvin1 - 273.15) * 1, 8 + 32));
         break;
 
 }
+Console.ReadKey();
