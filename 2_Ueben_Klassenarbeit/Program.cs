7c467aea649994dd483af1920c68450bd0f7d3ae
Mathias Scheer <mathias.s4@gso.schule.koeln>
2022-09-07 14:48:16 +0200

    3.2

---
+++ 2_Ueben_Klassenarbeit/Program.cs
@@ -0,0 +1,13 @@
+﻿double höhe_double = 32.346;
+double breite_double  = 15.193;
+
+string höhe_str = "Höhe";
+string breite_str = "Breite";
+
+
+Console.WriteLine("{0,10}{1,10}\n",höhe_str,breite_str);
+
+for(int i =0; i<100; i++)
+{
+    Console.WriteLine("{0,10}{1,10}", höhe_str, breite_str);
+}
